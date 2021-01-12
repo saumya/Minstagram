9239bbbdcfa88e993091beeee36a60bb62bd3141
saumya <2saumyaray@gmail.com>
2021-01-12 13:50:16 +0530

    wip: settings

--- js/settings.js
+++ js/settings.js
@@ -4,4 +4,14 @@
 
 (message=>{
     console.log('Settings version', message);
+
+    const btnInstallDB = document.getElementById('id_btn_installDB');
+
+    btnInstallDB.addEventListener('click', event=>{
+        event.preventDefault();
+        console.log('Initialise Database');
+        // TODO:
+        
+    });
+
 })('1.0.0')
