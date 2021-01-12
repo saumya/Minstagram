1916b20489e8ee45205e56edc0cd90a6078de43e
saumya <2saumyaray@gmail.com>
2021-01-12 15:38:50 +0530

    wip: settings

--- js/settings.js
+++ js/settings.js
@@ -7,9 +7,15 @@
 
     const btnInstallDB = document.getElementById('id_btn_installDB');
 
+    const init_db_url = 'init_db.php';
+
     const installDB = function(){
         console.log('installDB');
         //TODO: call server for the DB initialisation
+        console.log('dbURL', init_db_url);
+        const pData = {
+            'name': 'MinstagramUI'
+        };
     };
     // Event Handlers
     btnInstallDB.addEventListener('click', event=>{
